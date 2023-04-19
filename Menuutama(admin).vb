90778cd8809d4a9f7b8c1f91189288ee288c481b
Venomz22 <93197543+Venomz22@users.noreply.github.com>
2023-04-20 02:36:18 +0800

    desain menutama

---
+++ Menuutama(admin).vb
@@ -0,0 +1,5 @@
+﻿Public Class Menuutama_Admin_
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        End
+    End Sub
+End Class
---
+++ Menuutama(user).vb
@@ -0,0 +1,14 @@
+﻿Public Class Menuutama_user_
+    Private Sub Menuutama_user__Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Menuutama_Admin_.Show()
+        Me.Hide()
+    End Sub
+End Class
