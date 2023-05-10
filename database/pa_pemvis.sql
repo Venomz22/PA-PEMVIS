27f53cab6259f5940c39b2fb52449754250f6e96
PA-Pemvis <herby.jan1102@gmail.com>
2023-05-10 08:02:36 +0800

    New database

--- database/pa_pemvis.sql
+++ database/pa_pemvis.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Bulan Mei 2023 pada 08.52
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 7.4.30
+-- Generation Time: May 10, 2023 at 02:00 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `akun`
+-- Table structure for table `akun`
 --
 
 CREATE TABLE `akun` (
@@ -36,7 +36,39 @@ CREATE TABLE `akun` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `tbbuku`
+--
+
+CREATE TABLE `tbbuku` (
+  `idbuku` text NOT NULL,
+  `judul_buku` text NOT NULL,
+  `tahun_terbit` int(11) NOT NULL,
+  `pengarang` text NOT NULL,
+  `penerbit` text NOT NULL,
+  `jenis_buku` text NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga_buku` int(11) NOT NULL,
+  `jumlah_terjual` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbbuku`
+--
+
+INSERT INTO `tbbuku` (`idbuku`, `judul_buku`, `tahun_terbit`, `pengarang`, `penerbit`, `jenis_buku`, `jumlah`, `harga_buku`, `jumlah_terjual`) VALUES
+('ff', '400', 2, '2', '2', 'Petualangan', 2, 2, 0),
+('hh', 'tt', 0, 'w', 'w', '4', 6666, 3, 0),
+('hhhh', '1', 1, '1', '1', 'Petualangan', 1, 1, 1),
+('lllllll', 'ggg', 0, 'gg', 'gg', 'Petualangan', 0, 0, 0),
+('nn', '1', 1, '1', '1', 'Sejarah', 1, 1, 1),
+('rr', 'rr', 6, '5', '5', 'Fiksi Ilmiah', 55, 5, 5),
+('vv', '1', 1, '1', '1', 'Fiksi', 1, 1, 1),
+('zz', '1', 1, '1', '1', 'Drama', 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -52,23 +84,29 @@ CREATE TABLE `user` (
 --
 
 --
--- Indeks untuk tabel `akun`
+-- Indexes for table `akun`
 --
 ALTER TABLE `akun`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `tbbuku`
+--
+ALTER TABLE `tbbuku`
+  ADD PRIMARY KEY (`idbuku`(20));
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
